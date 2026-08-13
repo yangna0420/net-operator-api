7f4243ccfdb1c169906ac323951c5a312293bc46
Ian Gann <ian.gann@broadcom.com>
2026-08-13 09:25:53 -0700

    Update copyright notices

--- pkg/sdk/v1alpha1/doc.go
+++ pkg/sdk/v1alpha1/doc.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2026 Broadcom. All Rights Reserved.
-// Broadcom Confidential. The term "Broadcom" refers to Broadcom Inc.
-// and/or its subsidiaries.
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
 
 // Package v1alpha1 provides functions that assist in interacting with and traversing
 // Net Operator v1alpha1 resources in a Kubernetes cluster.
