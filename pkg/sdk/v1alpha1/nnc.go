9bf577811e0f87dae28954795b6740497ca97265
Ian Gann <ian.gann@broadcom.com>
2026-08-13 09:27:20 -0700

    Minor refactor

--- pkg/sdk/v1alpha1/nnc.go
+++ pkg/sdk/v1alpha1/nnc.go
@@ -46,8 +46,6 @@ func NetworkNamespaceNetworkConfigurationOwner(network *netopv1alpha1.Network) (
 //	// Search within a specific namespace
 //	nets, err := NetworksOwnedByNamespaceNetworkConfiguration(ctx, c, "my-nnc", "default")
 func NetworksOwnedByNamespaceNetworkConfiguration(ctx context.Context, c ctrlclient.Client, nncName, namespace string) ([]netopv1alpha1.Network, error) {
-	var networkList netopv1alpha1.NetworkList
-
 	listOpts := []ctrlclient.ListOption{
 		ctrlclient.MatchingLabels{netopv1alpha1.ManagedByNNCLabelKey: nncName},
 	}
@@ -57,6 +55,7 @@ func NetworksOwnedByNamespaceNetworkConfiguration(ctx context.Context, c ctrlcli
 		listOpts = append(listOpts, ctrlclient.InNamespace(namespace))
 	}
 
+	var networkList netopv1alpha1.NetworkList
 	if err := c.List(ctx, &networkList, listOpts...); err != nil {
 		return nil, fmt.Errorf("error listing Networks owned by NamespaceNetworkConfiguration '%s' in namespace '%s': %w", nncName, namespace, err)
 	}
